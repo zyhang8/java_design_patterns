248f3296d483e7b086827e7e0291c4ed83fe3019
zyh <1023708557@qq.com>
2019-05-28 21:06:00 +0800

    update

--- chapter21/21.2/Application.java
+++ chapter21/21.2/Application.java
@@ -1,7 +1,7 @@
 import java.io.File;
 public class Application{
      public static void main(String args[]) {
-          File dir=new File("d:/javaExample");
+          File dir=new File("/usr/local/bin");
          AbstractTemplate  template=new ConcreteTemplate1(dir);
          System.out.println(dir.getPath()+"目录下的文件：");
          template.showFileName();
